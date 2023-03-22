2f45dbdbf4435fdceef9bce8ae1b50db6d6ae2fe
Ankit2820 <102996168+Ankit2820@users.noreply.github.com>
2023-03-22 15:25:59 +0530

    Update Hotel.jsx

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -45,7 +45,7 @@ const Hotel = () => {
   const handleMove = (direction) =>{
     let newSlideNumber;
 
-    if(direction=="l"){
+    if(direction==="l"){
       newSlideNumber = slideNumber === 0 ? 5 : slideNumber - 1;
     }else{
       newSlideNumber = slideNumber === 5 ? 0 : slideNumber + 1;
